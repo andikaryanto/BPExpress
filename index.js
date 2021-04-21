5d0d4e5714f1f5aecb6c910168147b3b4be5ad5d
andik aryanto <andik.aryantoo@gmail.com>
2021-04-21 21:48:34 +0700

    remove

--- index.js
+++
@@ -1,7 +0,0 @@
-// Importing express in our app
-
-import Routes from "./App/Config/Routes.js";
-
-Routes(app => {
-     app.listen(3000, () => console.log("Server Up And Running. Listening On Port 3000"));
-});
